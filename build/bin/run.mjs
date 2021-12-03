92eedd2326b98e4a49d5bb6e5801e36f8ea059af
David Evans <davidje13@users.noreply.github.com>
2021-12-03 00:03:48 +0000

    Fix fast browser events not being consumed

--- build/bin/run.mjs
+++ build/bin/run.mjs
@@ -416,10 +416,11 @@ class EventListener {
 	}
 
 	addListener(id, fn) {
-		this.listeners.set(String(id), fn);
+		const normID = String(id);
+		this.listeners.set(normID, fn);
 		const events = [];
 		for (let i = 0; i < this.eventQueue.length; ++i) {
-			if (this.eventQueue[i].id === id) {
+			if (this.eventQueue[i].id === normID) {
 				events.push(...this.eventQueue[i].events);
 				this.eventQueue.splice(i, 1);
 				--i;
@@ -429,11 +430,12 @@ class EventListener {
 	}
 
 	handle({ id, events }) {
-		const listener = this.listeners.get(String(id));
+		const normID = String(id);
+		const listener = this.listeners.get(normID);
 		if (listener) {
 			events.forEach(listener);
 		} else {
-			this.eventQueue.push({ id, events });
+			this.eventQueue.push({ id: normID, events });
 		}
 	}
 }
--- src/bin/browser/EventListener.mjs
+++ src/bin/browser/EventListener.mjs
@@ -7,10 +7,11 @@ export default class EventListener {
 	}
 
 	addListener(id, fn) {
-		this.listeners.set(String(id), fn);
+		const normID = String(id);
+		this.listeners.set(normID, fn);
 		const events = [];
 		for (let i = 0; i < this.eventQueue.length; ++i) {
-			if (this.eventQueue[i].id === id) {
+			if (this.eventQueue[i].id === normID) {
 				events.push(...this.eventQueue[i].events);
 				this.eventQueue.splice(i, 1);
 				--i;
@@ -20,11 +21,12 @@ export default class EventListener {
 	}
 
 	handle({ id, events }) {
-		const listener = this.listeners.get(String(id));
+		const normID = String(id);
+		const listener = this.listeners.get(normID);
 		if (listener) {
 			events.forEach(listener);
 		} else {
-			this.eventQueue.push({ id, events });
+			this.eventQueue.push({ id: normID, events });
 		}
 	}
 }
