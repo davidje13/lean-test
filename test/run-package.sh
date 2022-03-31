afccf67d7ac8e61b9661d771ce6a39a3df5ba83c
David Evans <davidje13@users.noreply.github.com>
2022-03-31 15:26:48 +0100

    Remove version from package test files

--- test/run-package.sh
+++ test/run-package.sh
@@ -8,14 +8,14 @@ BASE_DIR="$(cd "$(dirname "$0")/.."; pwd)";
 cd "$BASE_DIR";
 rm /lean-test-*.tgz 2>/dev/null || true;
 npm pack;
-rm test/package/lean-test-*.tgz 2>/dev/null || true;
-mv lean-test-*.tgz test/package/;
+rm test/package/lean-test.tgz 2>/dev/null || true;
+mv lean-test-*.tgz test/package/lean-test.tgz;
 cd - >/dev/null;
 
 cd "$BASE_DIR/test/package";
 rm -rf node_modules || true;
-npm install --save-dev lean-test-*.tgz --audit=false;
-rm lean-test-*.tgz || true;
+npm install --audit=false;
+rm lean-test.tgz || true;
 npm test --ignore-scripts=false; # ignore-scripts is over-zealous on Node 14
 cd - >/dev/null;
 
